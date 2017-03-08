58a7d028180865efcbae366730d6741436854059
gvzq <gvzq@hotmail.com>
2017-03-07 21:57:08 -0600

    header id's updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   get 'contact/', to: 'root#contact'
+  get 'about/', to: 'root#about'
+  get 'sponsor/', to: 'root#sponsor'
+  get 'join/', to: 'root#join'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'root#index'
