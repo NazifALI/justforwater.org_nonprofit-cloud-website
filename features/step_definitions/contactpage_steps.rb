4a944bf32d7a19a250f057e866a406cec7da4796
gvzq <gvzq@hotmail.com>
2017-03-20 18:33:54 -0500

    steps contact

--- features/step_definitions/contactpage_steps.rb
+++ features/step_definitions/contactpage_steps.rb
@@ -1,12 +1,12 @@
 # features/step_definitions/contactpage_steps.rb
-Given(/^I am on contactpage$/) do
+Given(/^I am on the contactpage$/) do
   visit('/contact')
 end
 
 When(/^I click home$/) do
-  click_link('btn-home')
+  click_link('header-logo')
 end
 
-Then(/^I should see a home page$/) do
-  page.has_content?("The page you were looking for doesn't exist.")
+Then(/^I should see the home page$/) do
+   page.has_content?('Shi') # Write code here that turns the phrase above into concrete actions
 end
